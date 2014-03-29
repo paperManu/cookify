70f5171b503a99aec009a94f7599f50dd0183726
Emmanuel Durand <emmanueldurand@gmail.com>
2014-03-29 17:06:40 -0400

    Whut

--- cookie.js
+++ cookie.js
@@ -2,18 +2,20 @@ paper.install(window);
 
 var isMouseDown = false;
 var path;
+var icing;
+
 var vermiColors = ['#3FA9F5', '#7AC943', '#FF1D25', '#FF7BAC'];
 var qty = 10;
 var spread = 80;
 var length = 10; 
 
 function vermiColor () {
-	var a = Math.ceil(Math.random() * 3);
-	return vermiColors[a];
+    var a = Math.ceil(Math.random() * 3);
+    return vermiColors[a];
 }
 
 function vermiAngle () {
-	return Math.ceil(Math.random() * 360);
+    return Math.ceil(Math.random() * 360);
 }
 
 function vermicelles (x,y) {
@@ -43,8 +45,17 @@ window.onmousemove = function(ev) {
 window.onmouseup = function(ev) {
     isMouseDown = false;
     path.selected = false;
-    path.simplify(100);
+    path.simplify(10);
     path.smooth();
+
+    icing = path.clone();
+    icing.strokeWidth = 10;
+    for (var s in icing.segments) {
+        var x = Math.random() * 20 - 10;
+        var y = Math.random() * 20 - 10;
+        icing.segments[s].point._x += x;
+        icing.segments[s].point._y += y;
+    }
 }
 
 /*************/
